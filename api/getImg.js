f120623477f0bc8171096e993226816984447376
ahisna <138838948+chenleidaye@users.noreply.github.com>
2025-07-28 10:54:43 +0800

    Update getImg.js

--- api/getImg.js
+++ api/getImg.js
@@ -1,34 +1,36 @@
-var imgUrl = "https://image.tmdb.org"
-const http = require('http');
 const axios = require('axios');
-const url = require('url');
-const common = require('../utility/common.js')
 
 module.exports = async (req, res) => {
-  var { url: requestUrl} = req;
-  // 重定向的`/img`必须去除
-  if (!requestUrl.startsWith("/img")) {
-    return;
-  }else{
-    requestUrl = requestUrl.replace(/^\/img/, '');
-  }
-  imgUrl = `https://image.tmdb.org${requestUrl}`;
-
   try {
-      // 使用axios获取远程图像数据
-      const response = await axios.get(imgUrl, { responseType: 'arraybuffer' });
+    let requestUrl = req.url;
 
-      // 设置响应头，告诉浏览器返回的是图片数据
-      res.writeHead(200, {
-          'Content-Type': 'image/jpeg',
-          'Content-Length': response.data.length
-      });
-
-      // 将获取到的图像数据直接输出到浏览器
-      res.end(response.data, 'binary');
-    } catch (error) {
-        console.error('Error fetching image:', error);
-        res.writeHead(500, { 'Content-Type': 'text/plain' });
-        res.end(`${error}`);
+    if (!requestUrl.startsWith('/img')) {
+      res.statusCode = 404;
+      res.end('Not Found');
+      return;
     }
+
+    // 去除 /img 前缀，剩下图片路径
+    requestUrl = requestUrl.replace(/^\/img/, '');
+
+    // TMDb 图片完整地址
+    const imgUrl = `https://image.tmdb.org${requestUrl}`;
+
+    // 请求图片数据，响应类型 arraybuffer 以二进制形式接收
+    const response = await axios.get(imgUrl, { responseType: 'arraybuffer' });
+
+    // 识别图片MIME类型：这里简单用jpeg，你可以按需扩展判断
+    res.writeHead(200, {
+      'Content-Type': 'image/jpeg',
+      'Content-Length': response.data.length,
+    });
+
+    // 发送图片二进制数据
+    res.end(response.data, 'binary');
+  } catch (error) {
+    console.error('[Image] Error fetching image:', error.message);
+    res.statusCode = 500;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end(`Error fetching image: ${error.message}`);
+  }
 };
